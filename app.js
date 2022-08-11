2f60b091732365776e01c33cf37aac272189f04d
Alyssa Pausanos <alyssapausanos@Alyssas-MacBook-Pro-2.local>
2022-08-11 18:26:10 +1200

    recent changes

--- app.js
+++ app.js
@@ -32,6 +32,12 @@ app.get("*", function (request, response) {
   response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
 
+if (process.env.NODE_ENV === "production"|| process.env.NODE_ENV === "staging") {
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+    });
+   }
 //const port = process.env.PORT || 8082;
 
 const PORT = process.env.PORT || 5000
