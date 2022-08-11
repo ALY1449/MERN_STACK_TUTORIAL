ba61018e91beb5849b235f9b9559e35075fa929b
Alyssa Pausanos <alyssapausanos@Alyssas-MacBook-Pro-2.local>
2022-08-11 14:58:43 +1200

    first commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 var cors = require('cors');
 
+
 // routes
 const books = require('./routes/api/books');
 
@@ -21,6 +22,18 @@ app.get('/', (req, res) => res.send('Hello world!'));
 // use Routes
 app.use('/api/books', books);
 
-const port = process.env.PORT || 8082;
+// Accessing the path module
+const path = require("path");
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
+//const port = process.env.PORT || 8082;
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
