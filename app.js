0df302667452e14584faa8ebd1eb2a5729d2f1a6
Alyssa Pausanos <alyssapausanos@Alyssas-MacBook-Pro-2.local>
2022-08-15 10:29:47 +1200

    commit message

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 var cors = require('cors');
 require("dotenv").config();
+const bodyParser = require("body-parser");
 
 // routes
 const books = require('./routes/api/books');
@@ -16,6 +17,8 @@ app.use(cors({ origin: true, credentials: true }));
 
 // Init Middleware
 app.use(express.json({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Hello world!'));
 
@@ -26,33 +29,22 @@ app.use('/api/books', books);
 const path = require("path");
 
 // Step 1:
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.resolve(__dirname, "./my-app/build")));
 // Step 2:
 app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+  response.sendFile(path.resolve(__dirname, "./app/build", "index.html"));
 });
 
 if (process.env.NODE_ENV === "production"|| process.env.NODE_ENV === "staging") {
-    app.use(express.static("client/build"));
+    app.use(express.static("my-app/build"));
     app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+    res.sendFile(path.join(__dirname + "/my-app/build/index.html"));
     });
 }
 
+
 //const port = process.env.PORT || 8082;
 
-require("dotenv").config()
-
-    mongoose
-     .connect(
-         process.env.MONGODB_CONNECTION_STRING,
-             {
-               useNewUrlParser: true,
-               useUnifiedTopology: true,
-             }
-     )
-     .then(() => console.log("MongoDB has been connected"))
-     .catch((err) => console.log(err));
 
 const PORT = process.env.PORT || 5000
 
